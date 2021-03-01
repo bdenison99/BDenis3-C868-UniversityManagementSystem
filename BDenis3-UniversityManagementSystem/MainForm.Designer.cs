67f949161a8d3fcbe763195d0c8a2d7c625545e5
Bart Denison <bdenison99@users.noreply.github.com>
2021-02-28 22:25:02 -0700

    Adding new forms, moving forms to a folder once they are close to complete.

---
+++ BDenis3-UniversityManagementSystem/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace BDenis3_UniversityManagementSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonManageUsers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonManageUsers
+            // 
+            this.buttonManageUsers.Location = new System.Drawing.Point(617, 48);
+            this.buttonManageUsers.Name = "buttonManageUsers";
+            this.buttonManageUsers.Size = new System.Drawing.Size(129, 23);
+            this.buttonManageUsers.TabIndex = 0;
+            this.buttonManageUsers.Text = "Manage Users";
+            this.buttonManageUsers.UseVisualStyleBackColor = true;
+            this.buttonManageUsers.Click += new System.EventHandler(this.buttonManageUsers_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonManageUsers);
+            this.Name = "MainForm";
+            this.Text = "University Management System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonManageUsers;
+    }
+}
